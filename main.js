a3bccbd39b1b7d5cad35dbb1f54b627b4faad892
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-08-20 07:58:10 -0700

    clean up dir structure

--- main.js
+++ main.js
@@ -205,10 +205,6 @@ module.exports = {
 
 var caeser = require('./caeser');
 
-//Get text from textarea
-//Add button listener
-//Show shift
-//show entropy
 var decode = function () {
     var cipherInput = document.getElementById('cipherInput').value;
     var decoded = caeser.decode(cipherInput);
--- src/ui.js
+++ src/ui.js
@@ -2,10 +2,6 @@
 
 var caeser = require('./caeser');
 
-//Get text from textarea
-//Add button listener
-//Show shift
-//show entropy
 var decode = function () {
     var cipherInput = document.getElementById('cipherInput').value;
     var decoded = caeser.decode(cipherInput);
